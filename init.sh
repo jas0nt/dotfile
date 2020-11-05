1572b0ea6209e3b41ad8e8028c2e21e69be538a0
Jas0n <taoeta@gmail.com>
2020-11-05 21:37:18 +0800

    rime

--- init.sh
+++ init.sh
@@ -39,18 +39,18 @@ function system_cfg() {
 }
 
 function pacman_cfg() {
-    # 修改pacman源为腾讯源，有时候更新系统会把mirrorlist覆盖了，备份一下，当发现下载速度奇慢无比时检查此项
-    echo 'Server = https://mirrors.cloud.tencent.com/manjaro/stable/$repo/$arch' | sudo tee /etc/pacman.d/mirrorlist
+    # 修改pacman源为aliyun，有时候更新系统会把mirrorlist覆盖了，备份一下，当发现下载速度奇慢无比时检查此项
+    echo 'Server = https://mirrors.aliyun.com/manjaro/stable/$repo/$arch' | sudo tee /etc/pacman.d/mirrorlist
     sudo cp /etc/pacman.d/mirrorlist{,.bak}
     # 或者直接修改/etc/pacman.conf可以解决此问题，就像init-for-myself.sh一样
-    # sudo sed -i '/^Include = /s/^.*$/Server = https:\/\/mirrors.cloud.tencent.com\/manjaro\/stable\/$repo\/$arch/' /etc/pacman.conf
+    # sudo sed -i '/^Include = /s/^.*$/Server = https:\/\/mirrors.aliyun.com\/manjaro\/stable\/$repo\/$arch/' /etc/pacman.conf
 
     # pacman彩色输出
     sudo sed -i "/^#Color/s/#//" /etc/pacman.conf
 
-    # 添加腾讯云的archlinuxcn源
+    # 添加aliyun的archlinuxcn源
     if ! grep -q archlinuxcn /etc/pacman.conf ; then
-        echo -e '[archlinuxcn]\nServer = https://mirrors.cloud.tencent.com/archlinuxcn/$arch' | sudo tee -a /etc/pacman.conf
+        echo -e '[archlinuxcn]\nServer = https://mirrors.aliyun.com/archlinuxcn/$arch' | sudo tee -a /etc/pacman.conf
     fi
 
     # 更新系统，并安装一些下载工具
@@ -66,6 +66,8 @@ function dot_cfg() {
 	rm -rf ~/.Xmodmap
 	ln -s "$dotfiles_dir"/.i3 ~/.i3
 	ln -s "$dotfiles_dir"/.Xmodmap ~/.Xmodmap
+	ln -s "$dotfiles_dir"/.xprofile ~/.xprofile
+	ln -s "$dotfiles_dir"/.pam_environment ~/.pam_environment
 }
 
 function vim_cfg() {
@@ -79,7 +81,7 @@ function vim_cfg() {
 function emacs_cfg() {
     yay -S --noconfirm emacs
 	rm -rf ~/.emacs.d
-	git clone https://github.com/jas0nt/.emacs.d.git ~/.emacs.d
+	git clone https://gitee.com/e7a/emacsd.git ~/.emacs.d
 }
 
 function fish_cfg() {
@@ -90,8 +92,11 @@ function fish_cfg() {
 
 
 function im_cfg() {
-	yay -S --noconfirm fcitx-im fcitx-configtool
-	yay -S --noconfirm fcitx-googlepinyin
+	sudo pacman -Rs $(pacman -Qsq fcitx) #remove fcitx4
+	yay -S --noconfirm fcitx5-rime fcitx5-chinese-addons fcitx5-git fcitx5-gtk fcitx5-qt fcitx5-pinyin-zhwiki fcitx5-configtool
+	makedir ~/.local/share/fcitx5/rime
+	ln -s "$dotfiles_dir"/rime/default.custom.yaml ~/.local/share/fcitx5/rime/default.custom.yaml
+	ln -s "$dotfiles_dir"/rime/double_pinyin_flypy.schema.yaml ~/.local/share/fcitx5/rime/double_pinyin_flypy.schema.yaml
 
 	ln -s "$dotfiles_dir"/.xprofile ~/.xprofile
 	ln -s "$dotfiles_dir"/.profile ~/.profile
@@ -100,13 +105,11 @@ function im_cfg() {
 function cli_cfg() {
     # 安装say, see, terminal-tmux.sh，以及用于say, see修改和查看的cheat-sheets
     cp -v bin/* ~/.local/bin
-    makedir ~/.cheat
-    cp -v cheat/* ~/.cheat
 
     # CLI工具
     yay -S --noconfirm htop iotop dstat cloc screenfetch figlet cmatrix
     pip install pip -U
-    pip config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple
+    pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 }
 
 function desktop_cfg() {
