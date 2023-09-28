18d0539f0b6015253393132f08f287bb953b927d
Jas0nt <taoeta@gmail.com>
2023-09-28 21:36:21 +0800

    a

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+alias fuckGFW='export http_proxy=http://127.0.0.1:7890; and export https_proxy=http://127.0.0.1:7890'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
--- init.sh
+++ init.sh
@@ -39,19 +39,12 @@ function system_cfg() {
 }
 
 function pacman_cfg() {
-    # 修改pacman源为aliyun，有时候更新系统会把mirrorlist覆盖了，备份一下，当发现下载速度奇慢无比时检查此项
-    echo 'Server = https://mirrors.aliyun.com/manjaro/stable/$repo/$arch' | sudo tee /etc/pacman.d/mirrorlist
+    # 修改pacman源为tuna，有时候更新系统会把mirrorlist覆盖了，备份一下，当发现下载速度奇慢无比时检查此项
+    echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist
     sudo cp /etc/pacman.d/mirrorlist{,.bak}
-    # 或者直接修改/etc/pacman.conf可以解决此问题，就像init-for-myself.sh一样
-    # sudo sed -i '/^Include = /s/^.*$/Server = https:\/\/mirrors.aliyun.com\/manjaro\/stable\/$repo\/$arch/' /etc/pacman.conf
 
     # pacman彩色输出
     sudo sed -i "/^#Color/s/#//" /etc/pacman.conf
-
-    # 添加aliyun的archlinuxcn源
-    if ! grep -q archlinuxcn /etc/pacman.conf ; then
-        echo -e '[archlinuxcn]\nServer = https://mirrors.aliyun.com/archlinuxcn/$arch' | sudo tee -a /etc/pacman.conf
-    fi
 
     # 更新系统，并安装一些下载工具
     sudo pacman -Syyu --noconfirm
@@ -62,17 +55,21 @@ function pacman_cfg() {
 }
 
 function dot_cfg() {
-	yay -S --noconfirm i3 i3exit i3status i3lock i3blocks dmenu pcmanfm rofi awesome qtile alacritty
+	yay -S --noconfirm awesome dmenu dolphin rofi alacritty
 	backup ~/.config
 	backup ~/.Xmodmap
 	backup ~/.xprofile
 	backup ~/.pam_environment
 
-	ln -s "$dotfiles_dir"/.config ~/.config
-	chmod -R +x ~/.config/i3/scripts
-	ln -s "$dotfiles_dir"/.Xmodmap ~/.Xmodmap
-	ln -s "$dotfiles_dir"/.xprofile ~/.xprofile
-	ln -s "$dotfiles_dir"/.pam_environment ~/.pam_environment
+	# ln -s "$dotfiles_dir"/.config ~/.config
+	ln -s $dotfiles_dir/.config/alacritty ~/.config/alacritty
+	ln -s $dotfiles_dir/.config/awesome ~/.config/awesome
+	ln -s $dotfiles_dir/.config/dunst ~/.config/dunst
+	ln -s $dotfiles_dir/.config/rofi ~/.config/rofi
+	ln -s $dotfiles_dir/.Xmodmap ~/.Xmodmap
+	ln -s $dotfiles_dir/.xprofile ~/.xprofile
+	ln -s $dotfiles_dir/.profile ~/.profile
+	ln -s $dotfiles_dir/.pam_environment ~/.pam_environment
 }
 
 function vim_cfg() {
@@ -99,26 +96,20 @@ function im_cfg() {
 	sudo pacman -Rs $(pacman -Qsq fcitx) #remove fcitx4
 	yay -S --noconfirm librime fcitx5-rime fcitx5-chinese-addons fcitx5-git fcitx5-gtk fcitx5-qt fcitx5-pinyin-zhwiki fcitx5-configtool
 	makedir ~/.local/share/fcitx5/rime
-	ln -s "$dotfiles_dir"/rime/default.custom.yaml ~/.local/share/fcitx5/rime/default.custom.yaml
-	ln -s "$dotfiles_dir"/rime/double_pinyin_flypy.schema.yaml ~/.local/share/fcitx5/rime/double_pinyin_flypy.schema.yaml
-	
-	ln -s "$dotfiles_dir"/.xprofile ~/.xprofile
-	ln -s "$dotfiles_dir"/.profile ~/.profile
+	ln -s $dotfiles_dir/rime/default.custom.yaml ~/.local/share/fcitx5/rime/default.custom.yaml
+	ln -s $dotfiles_dir/rime/double_pinyin_flypy.schema.yaml ~/.local/share/fcitx5/rime/double_pinyin_flypy.schema.yaml
 }
 
 function cli_cfg() {
-	# 安装say, see, terminal-tmux.sh，以及用于say, see修改和查看的cheat-sheets
-	cp -v bin/* ~/.local/bin
-	
 	# CLI工具
-	yay -S --noconfirm htop iotop dstat cloc screenfetch figlet cmatrix libinput xf86-input-libinput xorg-xinput xdotool ranger npm
+	yay -S --noconfirm htop neofetch figlet cmatrix libinput xf86-input-libinput xorg-xinput xdotool ranger npm
 	pip install pip -U
 	pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
 }
 
 function desktop_cfg() {
 	# 桌面应用
-	yay -S --noconfirm deepin.com.qq.office netease-cloud-music wps-office ttf-wps-fonts ttf-fira-code flameshot firefox guake xfce4-terminal devilspie feh
+	yay -S --noconfirm linuxqq netease-cloud-music wps-office ttf-wps-fonts ttf-fira-code flameshot firefox feh
 	
 	# 其它工具：多媒体播放、多媒体处理、多媒体录制、gif录制、字体修改
 	yay -S --noconfirm vlc ffmpeg obs-studio peek fontforge
@@ -127,10 +118,10 @@ function desktop_cfg() {
 function main() {
 	dotfiles_dir=$PWD
 	export dotfiles_dir
-	system_cfg
+	# system_cfg
 	pacman_cfg
 	dot_cfg
-	vim_cfg
+	# vim_cfg
 	fish_cfg
 	im_cfg
 	cli_cfg
