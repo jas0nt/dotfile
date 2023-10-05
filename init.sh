276462757229d7bd43916a8053546380a34c496d
JasonT <taoeta@gmail.com>
2023-10-05 16:49:09 +0800

    a

--- init.sh
+++ init.sh
@@ -109,7 +109,7 @@ function cli_cfg() {
 
 function desktop_cfg() {
 	# 桌面应用
-	yay -S --noconfirm linuxqq spotify wps-office ttf-wps-fonts ttf-fira-code flameshot firefox feh
+	yay -S --noconfirm linuxqq spotify wps-office ttf-wps-fonts nerd-fonts ttf-fira-code firefox feh
 	
 	# 其它工具：多媒体播放、多媒体处理、多媒体录制、gif录制、字体修改
 	yay -S --noconfirm vlc ffmpeg obs-studio peek fontforge
