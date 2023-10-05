0ab7dc396f45af5e46c2ac7df584a7fee4679e72
JasonT <taoeta@gmail.com>
2023-10-05 13:39:54 +0800

    a

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -456,7 +456,8 @@ awful.rules.rules = {
             buttons = clientbuttons,
             screen = awful.screen.preferred,
             placement = awful.placement.no_overlap + awful.placement.no_offscreen,
-            size_hints_honor = false
+            size_hints_honor = false,
+            maximized = false
         }
     },
 
