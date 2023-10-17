275b414d17b14e15e3ac23e52843559827ba758c
JasonT <taoeta@gmail.com>
2023-10-17 13:24:18 +0800

    layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -74,33 +74,33 @@ local modkey                           = "Mod4"
 local altkey                           = "Mod1"
 local terminal                         = "alacritty"
 local vi_focus                         = false -- vi-like client focus https://github.com/lcpz/awesome-copycats/issues/275
-local editor                           = os.getenv("EDITOR") or "nvim"
+local editor                           = os.getenv("EDITOR") or "vim"
 
 awful.util.terminal                    = terminal
 awful.util.tagnames                    = { "λ", "2", "3", "4", "5", "6", "7", "8", "9", "0" }
 awful.layout.layouts                   = {
     awful.layout.suit.tile,
+    awful.layout.suit.max,
     awful.layout.suit.floating,
-    --awful.layout.suit.tile.left,
-    --awful.layout.suit.tile.bottom,
-    --awful.layout.suit.tile.top,
-    --awful.layout.suit.fair,
-    --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max,
-    --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
-    --awful.layout.suit.corner.nw,
-    --awful.layout.suit.corner.ne,
-    --awful.layout.suit.corner.sw,
-    --awful.layout.suit.corner.se,
-    --lain.layout.cascade,
-    --lain.layout.cascade.tile,
-    --lain.layout.centerwork,
-    --lain.layout.centerwork.horizontal,
-    --lain.layout.termfair,
-    --lain.layout.termfair.center
+    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile.bottom,
+    -- awful.layout.suit.tile.top,
+    -- awful.layout.suit.fair,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.corner.nw,
+    -- awful.layout.suit.corner.ne,
+    -- awful.layout.suit.corner.sw,
+    -- awful.layout.suit.corner.se,
+    -- lain.layout.cascade,
+    -- lain.layout.cascade.tile,
+    -- lain.layout.centerwork,
+    -- lain.layout.centerwork.horizontal,
+    -- lain.layout.termfair,
+    -- lain.layout.termfair.center
 }
 
 -- lain.layout.termfair.nmaster           = 3
